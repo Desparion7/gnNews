2fcc48b9e9e4d889b2fde92f134610bf607462b6
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-03-22 10:36:40 +0100

    change fetch url

--- src/store/apiSlice.tsx
+++ src/store/apiSlice.tsx
@@ -4,12 +4,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const newsApi = createApi({
 	reducerPath: 'newsApi',
-	baseQuery: fetchBaseQuery({ baseUrl: 'https://newsapi.org' }),
+	baseQuery: fetchBaseQuery({ baseUrl: '' }),
 	tagTypes: ['News'],
 	endpoints: (builder) => ({
 		getAllNews: builder.query({
 			query: (id: string) => ({
-				url: `/v2/top-headlines?country=${id}&apiKey=6d5a24f1a7f84d97bdb01545079421af`,
+				url: `https://newsapi.org/v2/top-headlines?country=${id}&apiKey=6d5a24f1a7f84d97bdb01545079421af`,
 				method: 'GET',
 			}),
 			providesTags: [{ type: 'News', id: 'LIST' }],
